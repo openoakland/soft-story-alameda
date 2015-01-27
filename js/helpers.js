e9f7500876540bc25b46710706258acd0244a779
Dave Guarino <dave@codeforamerica.org>
2015-01-26 23:56:21 -0800

    Add tests for all existing statuses, and passing code

--- js/helpers.js
+++ js/helpers.js
@@ -1,12 +1,24 @@
 function getStatus(description) {
-  if(description.match(/REHABILITATION/)) {
-    return 'Retrofitted';
-  }
-  else if(description.match(/RETROFIT COMPLETED/)) {
-    return 'Retrofitted';
-  }
-  else if(description === "SOFT STORY BUILDING") {
+  if(description === "SOFT STORY BUILDING") {
     return 'Soft Story';
+  }
+  else if(description.match('POTENTIAL SOFT STORY BUILDING')) {
+    return 'Potential Soft Story';
+  }
+  else if(description.match(/NOT SOFT STORY/)) {
+    return 'Not Soft Story';
+  }
+  else if(description.match(/NOT A SOFT STORY/)) {
+    return 'Not Soft Story';
+  }
+  else if(description.match(/REMOVED FROM/)) {
+    return 'Not Soft Story';
+  }
+  else if(description.match(/REHABILITATION/)) {
+    return 'Retrofitted';
+  }
+  else if(description.match(/RETROFIT/)) {
+    return 'Retrofitted';
   }
   else {
     return undefined;
